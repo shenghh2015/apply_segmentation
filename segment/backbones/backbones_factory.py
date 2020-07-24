993522b98bce70a9d30caae408bd5b5a7bb67c87
shenghh <shenghh2015@gmail.com>
2020-07-24 12:54:16 -0500

    segmentation

--- segment/backbones/backbones_factory.py
+++ segment/backbones/backbones_factory.py
@@ -2,9 +2,6 @@ import copy
 import efficientnet.model as eff
 from classification_models.models_factory import ModelsFactory
 
-from . import inception_resnet_v2 as irv2
-from . import inception_v3 as iv3
-
 
 class BackbonesFactory(ModelsFactory):
     _default_feature_layers = {
@@ -12,45 +9,6 @@ class BackbonesFactory(ModelsFactory):
         # List of layers to take features from backbone in the following order:
         # (x16, x8, x4, x2, x1) - `x4` mean that features has 4 times less spatial
         # resolution (Height x Width) than input image.
-
-        # VGG
-        'vgg16': ('block5_conv3', 'block4_conv3', 'block3_conv3', 'block2_conv2', 'block1_conv2'),
-        'vgg19': ('block5_conv4', 'block4_conv4', 'block3_conv4', 'block2_conv2', 'block1_conv2'),
-
-        # ResNets
-        'resnet18': ('stage4_unit1_relu1', 'stage3_unit1_relu1', 'stage2_unit1_relu1', 'relu0'),
-        'resnet34': ('stage4_unit1_relu1', 'stage3_unit1_relu1', 'stage2_unit1_relu1', 'relu0'),
-        'resnet50': ('stage4_unit1_relu1', 'stage3_unit1_relu1', 'stage2_unit1_relu1', 'relu0'),
-        'resnet101': ('stage4_unit1_relu1', 'stage3_unit1_relu1', 'stage2_unit1_relu1', 'relu0'),
-        'resnet152': ('stage4_unit1_relu1', 'stage3_unit1_relu1', 'stage2_unit1_relu1', 'relu0'),
-
-        # ResNeXt
-        'resnext50': ('stage4_unit1_relu1', 'stage3_unit1_relu1', 'stage2_unit1_relu1', 'relu0'),
-        'resnext101': ('stage4_unit1_relu1', 'stage3_unit1_relu1', 'stage2_unit1_relu1', 'relu0'),
-
-        # Inception
-        'inceptionv3': (228, 86, 16, 9),
-        'inceptionresnetv2': (594, 260, 16, 9),
-
-        # DenseNet
-        'densenet121': (311, 139, 51, 4),
-        'densenet169': (367, 139, 51, 4),
-        'densenet201': (479, 139, 51, 4),
-
-        # SE models
-        'seresnet18': ('stage4_unit1_relu1', 'stage3_unit1_relu1', 'stage2_unit1_relu1', 'relu0'),
-        'seresnet34': ('stage4_unit1_relu1', 'stage3_unit1_relu1', 'stage2_unit1_relu1', 'relu0'),
-        'seresnet50': (246, 136, 62, 4),
-        'seresnet101': (552, 136, 62, 4),
-        'seresnet152': (858, 208, 62, 4),
-        'seresnext50': (1078, 584, 254, 4),
-        'seresnext101': (2472, 584, 254, 4),
-        'senet154': (6884, 1625, 454, 12),
-
-        # Mobile Nets
-        'mobilenet': ('conv_pw_11_relu', 'conv_pw_5_relu', 'conv_pw_3_relu', 'conv_pw_1_relu'),
-        'mobilenetv2': ('block_13_expand_relu', 'block_6_expand_relu', 'block_3_expand_relu',
-                        'block_1_expand_relu'),
 
         # EfficientNets
         'efficientnetb0': ('block6a_expand_activation', 'block4a_expand_activation',
@@ -73,8 +31,6 @@ class BackbonesFactory(ModelsFactory):
     }
 
     _models_update = {
-        'inceptionresnetv2': [irv2.InceptionResNetV2, irv2.preprocess_input],
-        'inceptionv3': [iv3.InceptionV3, iv3.preprocess_input],
 
         'efficientnetb0': [eff.EfficientNetB0, eff.preprocess_input],
         'efficientnetb1': [eff.EfficientNetB1, eff.preprocess_input],
@@ -85,10 +41,6 @@ class BackbonesFactory(ModelsFactory):
         'efficientnetb6': [eff.EfficientNetB6, eff.preprocess_input],
         'efficientnetb7': [eff.EfficientNetB7, eff.preprocess_input],
     }
-
-    # currently not supported
-    _models_delete = ['resnet50v2', 'resnet101v2', 'resnet152v2',
-                      'nasnetlarge', 'nasnetmobile', 'xception']
 
     @property
     def models(self):
